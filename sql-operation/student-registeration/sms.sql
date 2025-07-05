9f884a89f1d16babb8d929dc07c554740e736302
wintun-devop <wintun.hlaing@outlook.com>
2025-07-05 14:39:09 +0630

    update some

---
+++ sql-operation/student-registeration/sms.sql
@@ -0,0 +1,27 @@
+CREATE TABLE students (
+    StudentID VARCHAR(255) PRIMARY KEY,
+    FirstName
+    LastName
+)
+
+CREATE TABLE departments (
+    DepartmentID VARCHAR(255) PRIMARY KEY,
+
+)
+
+CREATE TABLE instructors(
+    TeacherID   VARCHAR(255) PRIMARY KEY,
+    FirstName   VARCHAR(50),
+    LastName    VARCHAR(50),
+    Email       VARCHAR(100),
+    PhoneNumber VARCHAR(100),
+    Rank        VARCHAR(100)
+)
+
+CREATE TABLE courses (
+
+)
+
+CREATE TABLE enrollments (
+
+)
