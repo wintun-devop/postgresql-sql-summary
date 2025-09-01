174a015285ae3a83135e176272db4a550cd25c7b
Win Tun Hlaing <71956577+wintun-devop@users.noreply.github.com>
2025-09-01 12:45:44 +0630

    Update e-commerce.sql

--- sql-operation/student-registeration/e-commerce.sql
+++ sql-operation/student-registeration/e-commerce.sql
@@ -1,7 +1,18 @@
 
+CREATE TABLE sss_inventory(
+  id         TEXT PRIMARY KEY,
+  email      VARCHAR(255) UNIQUE,
+  password   VARCHAR(255) UNIQUE
+);
+  
 CREATE TABLE sss_inventory (
   id          TEXT PRIMARY KEY,
   sku_code    VARCHAR(255) UNIQUE,
   description TEXT
-)
+);
 
+
+CREATE TABLE sss_order(
+  id          TEXT PRIMARY KEY,
+  user_id     TEXT  
+);
