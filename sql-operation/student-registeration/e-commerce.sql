feb40f8b23f3a7d73341807fb12b0b7ac5c84d67
Win Tun Hlaing <71956577+wintun-devop@users.noreply.github.com>
2025-09-01 12:56:45 +0630

    Update e-commerce.sql

--- sql-operation/student-registeration/e-commerce.sql
+++ sql-operation/student-registeration/e-commerce.sql
@@ -1,8 +1,7 @@
-
-CREATE TABLE sss_inventory(
-  id         TEXT PRIMARY KEY,
-  email      VARCHAR(255) UNIQUE,
-  password   VARCHAR(255) UNIQUE
+CREATE TABLE sss_users(
+  id        TEXT   PRIMARY KEY,
+  email     VARCHAR(255) UNIQUE NOT NULL,
+  password  VARCHAR(255) NOT NULL
 );
   
 CREATE TABLE sss_inventory (
@@ -15,4 +14,10 @@ CREATE TABLE sss_inventory (
 CREATE TABLE sss_order(
   id          TEXT PRIMARY KEY,
   user_id     TEXT  
+);
+
+CREATE TABLE sss_order_details(
+   id           TEXT PRIMARY KEY,
+   order_id     TEXT 
+   inventory_id TEXT
 );
