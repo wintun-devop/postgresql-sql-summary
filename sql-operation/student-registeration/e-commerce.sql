c106fe1e4e320ec068e7834393788c27ee5e83ce
Win Tun Hlaing <71956577+wintun-devop@users.noreply.github.com>
2025-09-01 13:16:07 +0630

    Update e-commerce.sql

--- sql-operation/student-registeration/e-commerce.sql
+++ sql-operation/student-registeration/e-commerce.sql
@@ -1,19 +1,21 @@
+BEGIN;
+
 -- USERS table
-CREATE TABLE sss_users(
+CREATE TABLE IF NOT EXISTS sss_users(
   id        TEXT   PRIMARY KEY,
   email     VARCHAR(255) UNIQUE NOT NULL,
   password  VARCHAR(255) NOT NULL
 );
 
 -- INVENTORY table
-CREATE TABLE sss_inventory (
+CREATE TABLE IF NOT EXISTS sss_inventory (
   id          TEXT PRIMARY KEY,
   sku_code    VARCHAR(255) UNIQUE,
   description TEXT
 );
 
 -- ORDERS table (One-to-Many from users to orders)
-CREATE TABLE sss_order(
+CREATE TABLE IF NOT EXISTS sss_order(
   id          TEXT PRIMARY KEY,
   user_id     TEXT, 
   -- Foreign key: Each order belongs to a user
@@ -21,7 +23,7 @@ CREATE TABLE sss_order(
 );
 
 
-CREATE TABLE sss_order_details(
+CREATE TABLE IF NOT EXISTS sss_order_details(
    id           TEXT PRIMARY KEY,
    order_id     TEXT, 
    inventory_id TEXT,
@@ -31,3 +33,5 @@ CREATE TABLE sss_order_details(
   FOREIGN KEY (order_id) REFERENCES sss_order(id) ON DELETE CASCADE,
   FOREIGN KEY (inventory_id) REFERENCES sss_inventory(id) ON DELETE CASCADE
 );
+
+COMMIT;
